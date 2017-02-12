5e16cc3cc54efc61a6d5ff35a71f972a7ff242fa
ehodzh <ehodzh@gmail.com>
2017-02-12 21:19:58 +0700

    cluster worker

---
+++ src/core/server/cluster/worker.ts
@@ -0,0 +1,40 @@
+let logger = require('log4js').getLogger('worker');
+
+const DEFAULT_FORCE_TIMEOUT = 1000;
+
+export class ClusterWorker {
+  private shuttingDown: boolean;
+
+  constructor(private config, private server) {
+    process.on('SIGINT', () => this.shutdown());
+  }
+
+  middleware(req, res, next) {
+    if (!this.shuttingDown) {
+      return next();
+    }
+    res.set('Connection', 'close');
+    res.status(503).send('Server is in the process of restarting.');
+  }
+
+  private shutdown() {
+    // Don't bother with graceful shutdown on development to speed up round trip
+    // if (!process.env.NODE_ENV) return process.exit(1);
+
+    if (this.shuttingDown) {
+      return;
+    }
+    this.shuttingDown = true;
+    logger.warn('Received kill signal (SIGTERM), shutting down');
+
+    setTimeout(() => {
+      logger.error('Could not close connections in time, forcefully shutting down');
+      process.exit(1);
+    }, this.config.forceTimeout || DEFAULT_FORCE_TIMEOUT);
+
+    this.server.close(() => {
+      logger.info('Closed out remaining connections.');
+      process.exit();
+    });
+  }
+}
--- src/core/server/web/index.ts
+++ src/core/server/web/index.ts
@@ -14,123 +14,121 @@ let log = require('log4js').getLogger('web');
 
 let app = express();
 let server = http.createServer(app);
-let web = {
-  express: app,
-  server: server,
-  init: initServer,
-  start: startServer,
-  use: app.use.bind(app),
-  routers: {},
-  route: route
-};
 
-export = web;
+class WebServer {
 
-function initServer(config) {
-  app.use(compression());
-  app.use(bodyParser.urlencoded(config.web.bodyParser.urlencoded));
-  app.use(bodyParser.json(config.web.bodyParser.json));
-  app.use(methodOverride());
-  app.use(passport.initialize());
+  express = app;
+  server = server;
+  routers = {};
 
-  if (config.web.log) {
-    app.use(morgan('dev', {
-      stream: {
-        write: (msg) => {
-          log.info(msg);
+  constructor() {
+  }
+
+  init(config) {
+    app.use(compression());
+    app.use(bodyParser.urlencoded(config.web.bodyParser.urlencoded));
+    app.use(bodyParser.json(config.web.bodyParser.json));
+    app.use(methodOverride());
+    app.use(passport.initialize());
+
+    if (config.web.log) {
+      app.use(morgan('dev', {
+        stream: {
+          write: (msg) => {
+            log.info(msg);
+          }
         }
+      }));
+    }
+
+    if (config.web.liveReload) {
+      app.use(require('connect-livereload')());
+    }
+
+    //app.use(require('./io'));
+  }
+
+  start(config, auth, callback) {
+
+    this.beforeStart(config, auth);
+
+    server.listen(config.http.port, err => {
+      if (err) {
+        return callback(err);
       }
-    }));
+
+      log.info(`HTTP server listening on ${config.http.host || '0.0.0.0'}:${config.http.port}`);
+
+      // app.cluster.sendOnline();
+
+      callback();
+    });
   }
 
-  if (config.web.liveReload) {
-    app.use(require('connect-livereload')());
-  }
+  beforeStart(config, auth) {
 
-  //app.use(require('./io'));
-}
+    // use api
 
-function startServer(config, auth, callback) {
+    registerApi(this.routers, this, '');
 
-  beforeStart(config, auth);
+    function registerApi(routers, router, routePath) {
+      Object.keys(routers).forEach(registerApiKey);
 
-  server.listen(config.http.port, function (err) {
-    if (err) {
-      return callback(err);
-    }
-
-    log.info('HTTP server listening on %d',
-      config.http.port);
-
-    // app.cluster.sendOnline();
-
-    callback();
-  });
-}
-
-function beforeStart(config, auth) {
-
-  // use api
-
-  registerApi(web.routers, web, '');
-
-  function registerApi(routers, router, routePath) {
-    Object.keys(routers).forEach(registerApiKey);
-
-    function registerApiKey(apiKey) {
-      let api = routers[apiKey];
-      let apiRouter = express.Router();
-      let apiPath = [routePath, apiKey].join('');
-      if (typeof api === 'function') {
-        api(apiRouter, auth);
-      } else {
-        registerApi(api, apiRouter, apiPath);
+      function registerApiKey(apiKey) {
+        let api = routers[apiKey];
+        let apiRouter = express.Router();
+        let apiPath = [routePath, apiKey].join('');
+        if (typeof api === 'function') {
+          api(apiRouter, auth);
+        } else {
+          registerApi(api, apiRouter, apiPath);
+        }
+        log.info('route', apiPath);
+        router.use(apiKey, apiRouter);
       }
-      log.info('route', apiPath);
-      router.use(apiKey, apiRouter);
+    }
+
+    if (config.static) {
+
+      // use static api
+
+      let staticRoot = path.resolve(config.static);
+      app.use(express.static(staticRoot));
+
+      // render index.html otherwise
+
+      app.get('/*', renderRoot);
+
+      log.info('serving static files from', staticRoot);
+    }
+
+    // error handler has to be last
+
+    app.use(onError);
+
+    function onError(err, req, res, next) {
+      log.error(err);
+      let code = 500;
+      let msg = {message: 'Internal Server Error'};
+      return res.status(code).json(msg);
+    }
+    function renderRoot(req, res) {
+      if (req.originalUrl !== '/') {
+        log.error(req.originalUrl, 'not found');
+      }
+      let indexPath = path.join(path.resolve(config.static), 'index.html');
+      log.trace(indexPath);
+      res.sendFile(indexPath);
     }
   }
 
-  if (config.static) {
-
-    // use static api
-
-    let staticRoot = path.resolve(config.static);
-    app.use(express.static(staticRoot));
-
-    // render index.html otherwise
-
-    app.get('/*', renderRoot);
-
-    log.info('serving static files from', staticRoot);
-  }
-
-  // error handler has to be last
-
-  app.use(onError);
-
-  function onError(err, req, res, next) {
-    log.error(err);
-    let code = 500;
-    let msg = {message: 'Internal Server Error'};
-    return res.status(code).json(msg);
-  }
-
-
-  function renderRoot(req, res) {
-    if (req.originalUrl !== '/') {
-      log.error(req.originalUrl, 'not found');
-    }
-    let indexPath = path.join(path.resolve(config.static), 'index.html');
-    log.trace(indexPath);
-    res.sendFile(indexPath);
+  route(routers) {
+    Object.keys(routers).forEach(
+      (key) => {
+        this.routers[key] = routers[key];
+      }
+    );
   }
 }
 
-function route(routers) {
-  Object.keys(routers).forEach(
-    function (key) {
-      web.routers[key] = routers[key];
-    }
-  );
-}
+export = new WebServer();
