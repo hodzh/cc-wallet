f68e0006050c9c065599cc1008433369ede4715d
ehodzh <ehodzh@gmail.com>
2017-11-03 15:39:53 +0700

    sample application

--- src/app/cc-wallet/server/modules.ts
+++
@@ -1,8 +0,0 @@
-export const MODULES = [
-  require('../../../core/server'),
-  require('../../../dev/server'),
-  require('../../../wallet/server'),
-  require('../../../games-video-poker/server'),
-  require('../../../paygates/server'),
-  require('../../../paygates-bitcoin/server')
-];
